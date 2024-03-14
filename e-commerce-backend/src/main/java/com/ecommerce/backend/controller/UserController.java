aabd70b984b1815f0f8cb507a1bde9f5c6b5c6f5
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-03-14 07:45:55 +0530

    Updated api end points in UserController class

--- e-commerce-backend/src/main/java/com/ecommerce/backend/controller/UserController.java
+++ e-commerce-backend/src/main/java/com/ecommerce/backend/controller/UserController.java
@@ -4,14 +4,12 @@ import com.ecommerce.backend.service.UserService;
 import com.ecommerce.backend.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
@@ -27,13 +25,13 @@ public class UserController {
         return userService.registerNewUser(user);
     }
 
-    @GetMapping({"/forAdmin"})
+    @GetMapping("/forAdmin")
     @PreAuthorize("hasRole('Admin')")
     public String forAdmin(){
         return "This URL is accessible for admin only!";
     }
 
-    @GetMapping({"/forUser"})
+    @GetMapping("/forUser")
     @PreAuthorize("hasRole('User')")
     public String forUser(){
         return "This URL is accessible for user only!";
