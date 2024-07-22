d032c3a860e06a37b58864eaca130df049dec7c7
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-07-22 22:49:28 +0530

    added onFileSelected function and prepareFormData function in add new product component ts file

--- e-commerce-ui/src/app/add-new-product/add-new-product.component.ts
+++ e-commerce-ui/src/app/add-new-product/add-new-product.component.ts
@@ -3,6 +3,8 @@ import { Product } from '../model/product.model';
 import { NgForm } from '@angular/forms';
 import { ProductService } from '../services/product.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { FileHandle } from '../model/file-handle.model';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add-new-product',
@@ -15,16 +17,22 @@ export class AddNewProductComponent implements OnInit {
     productDescription: '',
     productDiscountedPrice: 0,
     productActualPrice: 0,
+    productImages: [],
   };
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private domSanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
   addProduct(productForm: NgForm) {
-    this.productService.addProduct(this.product).subscribe({
+    const productFormData = this.prepareFormData(this.product);
+
+    this.productService.addProduct(productFormData).subscribe({
       next: (response: Product) => {
         productForm.reset();
       },
@@ -32,5 +40,35 @@ export class AddNewProductComponent implements OnInit {
         console.log(error);
       },
     });
+  }
+
+  prepareFormData(product: Product): FormData {
+    const formData = new FormData();
+    formData.append(
+      'product',
+      new Blob([JSON.stringify(product)], { type: 'application/json' })
+    );
+    for (var i = 0; i < product.productImages.length; i++) {
+      formData.append(
+        'imageFile',
+        product.productImages[i].file,
+        product.productImages[i].file.name
+      );
+    }
+    return formData;
+  }
+
+  onFileSelected(event: any) {
+    if (event.target.files) {
+      const file = event.target.files[0];
+
+      const fileHandle: FileHandle = {
+        file: file,
+        url: this.domSanitizer.bypassSecurityTrustUrl(
+          window.URL.createObjectURL(file)
+        ),
+      };
+      this.product.productImages.push(fileHandle);
+    }
   }
 }
