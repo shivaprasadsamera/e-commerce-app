611ac0b619a5ef279df12ddc48f7a0b8f2287f1e
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-07-22 18:30:30 +0530

    updated product service ts file

--- e-commerce-ui/src/app/services/product.service.ts
+++ e-commerce-ui/src/app/services/product.service.ts
@@ -3,13 +3,15 @@ import { Injectable } from '@angular/core';
 import { Product } from '../model/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  public addProduct(product: Product){
-    return this.httpClient.post<Product>("http://localhost:8585/addNewProduct",product);
+  public addProduct(product: Product) {
+    return this.httpClient.post<Product>(
+      'http://localhost:8585/addNewProduct',
+      product
+    );
   }
 }
