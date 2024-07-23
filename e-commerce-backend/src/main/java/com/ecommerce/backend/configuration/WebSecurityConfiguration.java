b576948bf5db412b7db73aa49c8002c429990d67
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-07-23 18:53:57 +0530

    Updated WebSecurityConfiguration.java class

--- e-commerce-backend/src/main/java/com/ecommerce/backend/configuration/WebSecurityConfiguration.java
+++ e-commerce-backend/src/main/java/com/ecommerce/backend/configuration/WebSecurityConfiguration.java
@@ -41,7 +41,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         httpSecurity.cors()
                 .and()
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate","/registerNewUser","/addNewProduct").permitAll()
+                .authorizeRequests().antMatchers("/authenticate","/registerNewUser","/getAllProducts","/addNewProduct").permitAll()
                 .antMatchers(HttpHeaders.ALLOW).permitAll()
                 .anyRequest().authenticated()
                 .and()
