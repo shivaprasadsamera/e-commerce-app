6d6fac7c54829f27517e5df44f66f84af5a7913d
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-07-23 18:53:18 +0530

    Updated ProductController.java class

--- e-commerce-backend/src/main/java/com/ecommerce/backend/controller/ProductController.java
+++ e-commerce-backend/src/main/java/com/ecommerce/backend/controller/ProductController.java
@@ -5,15 +5,14 @@ import com.ecommerce.backend.entity.Product;
 import com.ecommerce.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.*;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -22,10 +21,11 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping(value = "/addNewProduct" , consumes={MediaType.MULTIPART_FORM_DATA_VALUE})
+
+//    @PreAuthorize("hasRole('Admin')")
+    @PostMapping(value = {"/addNewProduct"} , consumes={MediaType.MULTIPART_FORM_DATA_VALUE})
     public Product addNewProduct(@RequestPart("product") Product product,
                                  @RequestPart("imageFile") MultipartFile[] file) {
-//        return productService.addNewProduct(product);
 
         try {
             Set<ImageModel> images = uploadImage(file);
@@ -49,6 +49,11 @@ public class ProductController {
             imageModels.add(imageModel);
         }
         return imageModels;
+    }
+
+    @GetMapping("/getAllProducts")
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
     }
 
 }
