99640b59de8bd6401a18e0267e6e108ad6a1aa22
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-07-25 00:03:14 +0530

    removed #PreAuthorize annotation for getAllProducts API

--- e-commerce-backend/src/main/java/com/ecommerce/backend/controller/ProductController.java
+++ e-commerce-backend/src/main/java/com/ecommerce/backend/controller/ProductController.java
@@ -51,7 +51,7 @@ public class ProductController {
         return imageModels;
     }
 
-    @PreAuthorize("hasRole('Admin')")
+
     @GetMapping("/getAllProducts")
     public List<Product> getAllProducts(){
         return productService.getAllProducts();
