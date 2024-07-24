987c9e980d8b428fddd7903089e0a695759fcd7c
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-07-25 00:08:34 +0530

    Added getAllProducts function in home component ts file

--- e-commerce-ui/src/app/home/home.component.ts
+++ e-commerce-ui/src/app/home/home.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { ImageProcessingService } from '../services/image-processing.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Product } from '../model/product.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  productDetails: Product[] = [];
 
+  constructor(
+    private productService: ProductService,
+    private imageProcessingService: ImageProcessingService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+    throw new Error('Method not implemented.');
+  }
+
+  public getAllProducts() {
+    this.productService
+      .getAllProducts()
+      .pipe(
+        map((x: Product[], i) =>
+          x.map((product: Product) =>
+            this.imageProcessingService.createImages(product)
+          )
+        )
+      )
+      .subscribe({
+        next: (response: Product[]) => {
+          this.productDetails = response;
+        },
+        error: (error: HttpErrorResponse) => {
+          console.log(error);
+        },
+      });
+  }
 }
