c4aba0cb4b5dda67844f38997794c301f70c2b89
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-08-08 01:44:32 +0530

    Created getCartDetails API endpoint and added @RequestMapping annotation to the controller

--- e-commerce-backend/src/main/java/com/ecommerce/backend/controller/CartController.java
+++ e-commerce-backend/src/main/java/com/ecommerce/backend/controller/CartController.java
@@ -3,12 +3,15 @@ package com.ecommerce.backend.controller;
 import com.ecommerce.backend.entity.Cart;
 import com.ecommerce.backend.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/cart")
 public class CartController {
 
     private final CartService cartService;
@@ -22,6 +25,12 @@ public class CartController {
     @GetMapping("/addToCart/{productId}")
     public Cart addToCart(@PathVariable(name = "productId") Integer productId){
         return cartService.addToCart(productId);
+    }
+
+    @PreAuthorize("hasRole('User')")
+    @GetMapping("/getCartDetails")
+    public List<Cart> getCartDetails(){
+        return cartService.getCartDetails();
     }
 
 }
