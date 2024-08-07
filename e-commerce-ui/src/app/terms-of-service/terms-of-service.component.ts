7506575668b76e86a1c7406fc8c529606be16a0b
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-08-07 16:01:04 +0530

    added terms of services component

---
+++ e-commerce-ui/src/app/terms-of-service/terms-of-service.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-of-service',
+  templateUrl: './terms-of-service.component.html',
+  styleUrl: './terms-of-service.component.css',
+})
+export class TermsOfServiceComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
