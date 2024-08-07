c3bd140eff4472b1a2807affcf3bed1b6d3b618a
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-08-08 01:44:54 +0530

    updated

--- e-commerce-backend/src/main/java/com/ecommerce/backend/dao/CartDao.java
+++ e-commerce-backend/src/main/java/com/ecommerce/backend/dao/CartDao.java
@@ -1,9 +1,16 @@
 package com.ecommerce.backend.dao;
 
 import com.ecommerce.backend.entity.Cart;
+import com.ecommerce.backend.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartDao extends CrudRepository<Cart, Integer> {
+
+    public List<Cart> findByUser(User user);
+
+
 }
