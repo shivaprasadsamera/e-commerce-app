b84445551aac5b062300a124ff0adf11a0e0b0f2
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-08-09 03:08:16 +0530

    added @RequestMapping annotation to the controller and  added deleteCartItem api endpoint in cart controller

--- e-commerce-backend/src/main/java/com/ecommerce/backend/controller/CartController.java
+++ e-commerce-backend/src/main/java/com/ecommerce/backend/controller/CartController.java
@@ -3,8 +3,6 @@ package com.ecommerce.backend.controller;
 import com.ecommerce.backend.entity.Cart;
 import com.ecommerce.backend.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +24,13 @@ public class CartController {
     public Cart addToCart(@PathVariable(name = "productId") Integer productId){
         return cartService.addToCart(productId);
     }
+
+    @PreAuthorize("hasRole('User')")
+    @DeleteMapping("/deleteCartItem/{cartId}")
+    public void deleteCartItem(@PathVariable(name = "cartId") Integer cartId){
+        cartService.deleteCartItem(cartId);
+    }
+
 
     @PreAuthorize("hasRole('User')")
     @GetMapping("/getCartDetails")
