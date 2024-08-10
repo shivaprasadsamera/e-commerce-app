e056797a5b1c0a892036e33d8af55f04fad83372
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-08-10 15:57:39 +0530

    added OrderDetailsDao class

--- e-commerce-backend/src/main/java/com/ecommerce/backend/dao/OrderDetailsDao.java
+++ e-commerce-backend/src/main/java/com/ecommerce/backend/dao/OrderDetailsDao.java
@@ -1,7 +1,11 @@
 package com.ecommerce.backend.dao;
 
 import com.ecommerce.backend.entity.OrderDetails;
+import com.ecommerce.backend.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderDetailsDao extends CrudRepository<OrderDetails, Integer> {
+    List<OrderDetails> findByUser(User user);
 }
