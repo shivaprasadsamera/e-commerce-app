5365761dcd136f2ff5a0848d1c72ab858067720a
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-08-15 23:20:15 +0530

    Created ContactForm entity class

---
+++ e-commerce-backend/src/main/java/com/ecommerce/backend/entity/ContactForm.java
@@ -0,0 +1,56 @@
+package com.ecommerce.backend.entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "contact_form")
+public class ContactForm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    private String message;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
