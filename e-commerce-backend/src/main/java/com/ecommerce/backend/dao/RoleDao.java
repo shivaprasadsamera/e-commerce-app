4d0544b6b9e681d4ce90377573c927d56070cfc5
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-08-15 23:28:17 +0530

    updated

--- e-commerce-backend/src/main/java/com/ecommerce/backend/dao/RoleDao.java
+++ e-commerce-backend/src/main/java/com/ecommerce/backend/dao/RoleDao.java
@@ -1,9 +1,9 @@
 package com.ecommerce.backend.dao;
 
 import com.ecommerce.backend.entity.Role;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleDao extends CrudRepository<Role, String> {
+public interface RoleDao extends JpaRepository<Role, String> {
 }
